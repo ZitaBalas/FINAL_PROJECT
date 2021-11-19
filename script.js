3888dee30628838cb7c59aebc17bfc15e66ee4f9
ZitaBalas <zetablasius@gmail.com>
2021-11-19 15:49:47 +0100

    display details on hover

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let curr = window.scrollY;
+const topBtn = document.getElementById("top");
+
+const displayTopBtn = () => {
+    if (window.scrollY > 1000 && window.scrollY !== curr) {
+        topBtn.classList.remove("fadeOut");
+        topBtn.style.visibility = "visible";
+
+        setTimeout(() => {
+            topBtn.classList.add("fadeOut");
+        }, 2000);
+        curr = window.scrollY;
+    }
+}
+window.onscroll = () => displayTopBtn();

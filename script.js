359243b1da38098419c6c4246d4150deb3ef505b
ZitaBalas <zetablasius@gmail.com>
2021-11-21 20:14:04 +0100

    add carousel

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 let curr = window.scrollY;
-const topBtn = document.getElementById("top");
+const topBtn = document.getElementById('top');
 
 const displayTopBtn = () => {
     if (window.scrollY > 1000 && window.scrollY !== curr) {
-        topBtn.classList.remove("fadeOut");
-        topBtn.style.visibility = "visible";
+        topBtn.classList.remove('fadeOut');
+        topBtn.style.visibility = 'visible';
 
         setTimeout(() => {
-            topBtn.classList.add("fadeOut");
+            topBtn.classList.add('fadeOut');
         }, 2000);
         curr = window.scrollY;
     }
